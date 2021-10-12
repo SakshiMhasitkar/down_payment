e7c44e748eea33d700c03f1f82811c0c68fe2c2b
Sakshi <92329377+SakshiMhasitkar@users.noreply.github.com>
2021-10-12 20:06:11 +0530

    Add files via upload

---
+++ down payment.py
@@ -0,0 +1,10 @@
+price = 10000
+good_credit = True
+
+if good_credit:
+    down_payment = 0.1 * price
+
+else:
+    down_payment = 0.2 * price
+
+print(f"The required down payment is: ${down_payment}")
